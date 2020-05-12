889138b5c2cea0577f2952af0d88b24d5aa98c39
haijiwuya <659395145@qq.com>
2020-05-12 22:50:32 +0800

    提交新增

--- base/4.dictionary/1.基本介绍.py
+++ base/4.dictionary/1.基本介绍.py
@@ -48,3 +48,45 @@ d = {"a": 1, 'b': 2, 'c': 3}
 d2 = {'d': 4, 'e': 5}
 d.update(d2)
 print(d)
+
+# 删除，可以使用del来删除字典中的key-value
+del d['a']
+print(d)
+del d['b']
+print(d)
+
+# popitem() 随机删除字典中的一个键值对，一般都会删除最后一个键值对,并且删除之后将删除的key-value作为返回值以元组的形式返回
+# popitem()删除一个空字典会抛出异常
+result = d.popitem()
+print(d)
+print(result)
+
+# pop(key[, default])
+# 根据key删除字典中的key-value，将被删除的value返回
+# 如果被删除的key不存在，则抛出异常，可以指定默认值再删除
+result = d.pop('d')
+d.pop('f', '这是默认值')
+print(result, d)
+
+# clear()用来清空字典
+d.clear()
+print(d)
+
+# copy() 用来对字典进行浅复制,复制以后的对象和原对象是独立的，修改一个不会影响另一个
+# 浅复制会简单复制对象内部的值，如果值是一个可变对象，这个可变对象不会被复制
+d = {"a": 1, 'b': 2, 'c': 3}
+print(d.copy())
+
+d = {'a': {'name': '孙悟空', 'age': 18}, 'b': 2, 'c': 3}
+d2 = d.copy()
+d2['a']['name'] = '猪八戒'
+print('d=', d, id(d))
+print('d2=', d2, id(d2))
+
+# 遍历字典
+d = {'a': {'name': '孙悟空', 'age': 18}, 'b': 2, 'c': 3}
+for k in d.keys():
+    print(k)
+    print(d[k])
+print(d.values())
+print(d.items())
